39d98f4fbd7d24699dfd7169b0a288a2bf24a698
Gabe Maentz <gabe@maentz.net>
2023-08-01 14:35:53 -0400

    update compute not to use workspace run

--- infrastructure/network/main.tf
+++ infrastructure/network/main.tf
@@ -38,22 +38,22 @@ module "vpc" {
   }
 }
 
-resource "tfe_workspace_run" "compute" {
-  workspace_id = tfe_workspace.clumsy-bird-compute.id
+# resource "tfe_workspace_run" "compute" {
+#   workspace_id = tfe_workspace.clumsy-bird-compute.id
 
-  depends_on = [module.vpc]
+#   depends_on = [module.vpc]
 
-  apply {
-    # Fire and Forget
-    wait_for_run = false
-    # auto-apply
-    manual_confirm = true
-  }
+#   apply {
+#     # Fire and Forget
+#     wait_for_run = false
+#     # auto-apply
+#     manual_confirm = true
+#   }
 
-  destroy {
-    # Wait for destroy before doing anything else
-    wait_for_run = true
-    # auto-apply
-    manual_confirm = true
-  }
-}
+#   destroy {
+#     # Wait for destroy before doing anything else
+#     wait_for_run = true
+#     # auto-apply
+#     manual_confirm = true
+#   }
+# }
