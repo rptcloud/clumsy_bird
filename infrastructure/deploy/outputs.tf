29ea36b5646aa39f1d4b35c4398314807f609beb
Gabe Maentz <gabe@maentz.net>
2023-08-02 12:52:28 -0400

    update outputs on deploy

--- infrastructure/deploy/outputs.tf
+++ infrastructure/deploy/outputs.tf
@@ -1,24 +1,15 @@
-# output "clumsy-bird-workspace-names" {
-#   value = data.tfe_workspace.ws[*].id
-# }
+output "clumsy-bird-workspace-names" {
+  value = [for p in local.workspaces : data.tfe_workspace.ws[p].name]
+}
 
-# output "clumsy-bird-url" {
-#   value = data.tfe_outputs.workspaces["clumsy-bird-compute"].values.clumsy-bird-url
-#   depends_on = [
-#     tfe_workspace_run.compute
-#   ]
-# }
+output "clumsy-bird-url" {
+  value = data.tfe_outputs.workspaces["clumsy-bird-compute"].nonsensitive_values.clumsy-bird-url
+}
 
-# output "clumsy-bird-ip" {
-#   value = data.tfe_outputs.workspaces["clumsy-bird-compute"].values.clumsy-bird-ip
-#   depends_on = [
-#     tfe_workspace_run.compute
-#   ]
-# }
+output "clumsy-bird-ip" {
+  value = data.tfe_outputs.workspaces["clumsy-bird-compute"].nonsensitive_values.clumsy-bird-ip
+}
 
-# output "s3_bucket" {
-#   value = data.tfe_outputs.workspaces["clumsy-bird-compute"].values.s3_bucket
-#   depends_on = [
-#     tfe_workspace_run.compute
-#   ]
-# }
+output "s3_bucket" {
+  value = data.tfe_outputs.workspaces["clumsy-bird-compute"].nonsensitive_values.s3_bucket
+}
