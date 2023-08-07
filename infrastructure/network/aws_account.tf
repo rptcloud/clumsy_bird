52853c4d45cf48a40a4b19ff5e39b2aeab99780a
Gabe Maentz <gabe@maentz.net>
2023-08-07 14:41:42 -0400

    update to reflect aws creds

--- infrastructure/network/aws_account.tf
+++ infrastructure/network/aws_account.tf
@@ -2,14 +2,14 @@
 data "aws_caller_identity" "inuse" {}
 
 # outputs
-output "account_id" {
+output "aws_account_id" {
   value = data.aws_caller_identity.inuse.account_id
 }
 
-output "caller_arn" {
+output "aws_caller_arn" {
   value = data.aws_caller_identity.inuse.arn
 }
 
-output "caller_user" {
+output "aws_caller_user" {
   value = data.aws_caller_identity.inuse.user_id
 }
